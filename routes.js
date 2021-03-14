b1baca79264fd5cb454acc8a3a1ab62136d4c1c2
tjfitzzster <tjfitzster@gmail.com>
2021-03-14 19:16:52 +0000

    I'm just gettign my signup and Login page setup.

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ const POI = require('./app/controllers/POI');
 
 module.exports = [
     { method: 'GET', path: '/', config: POI.index },
+    { method: "GET", path: "/signup", config: POI.signup },
+    { method: "GET", path: "/login", config: POI.login },
     {
         method: 'GET',
         path: '/{param*}',
